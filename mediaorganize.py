c71b736917f9e3f4702ef801d34d1404b2fc811b
Perihelion <nine-t-eight@protonmail.ch>
2021-08-03 18:29:16 -0400

    Update mediaorganize.py

--- mediaorganize.py
+++ mediaorganize.py
@@ -4,7 +4,7 @@ import os
 import datetime
 from time import strftime
 files = []
-supported = [".jpg", ".png", ".webm", ".jpeg", ".mp4", ".gif"]
+supported = [".jpg", ".png", ".webm", ".jpeg", ".mp4", ".gif", ".mov"]
 lsdir = os.listdir(path=".")
 print("Folder File Date Organizer")
 for x in lsdir:
@@ -18,15 +18,15 @@ for x in files:
     fm_month = filemtime.strftime("%B")
     fm_year = filemtime.strftime("%Y")
     try:
-        if not os.path.exists(fm_year):os.mkdir(fm_year)
-        if not os.path.exists(fm_year+"/"+fm_month):os.mkdir(fm_year+"/"+fm_month)
-        if not os.path.exists(fm_year+"/"+fm_month+"/"+fm_day):os.mkdir(fm_year+"/"+fm_month+"/"+fm_day)
-    except:
-        print("Failed to create a folder. Please check permissions.")
-        sys.exit()
-    try:
         filename, file_extension = os.path.splitext(x)
         if file_extension in supported:
+            try:
+                if not os.path.exists(fm_year):os.mkdir(fm_year)
+                if not os.path.exists(fm_year+"/"+fm_month):os.mkdir(fm_year+"/"+fm_month)
+                if not os.path.exists(fm_year+"/"+fm_month+"/"+fm_day):os.mkdir(fm_year+"/"+fm_month+"/"+fm_day)
+            except:
+                 print("Failed to create a folder. Please check permissions.")
+                 sys.exit()
             os.rename(x,fm_year+"/"+fm_month+"/"+fm_day+"/"+x)
             print("Successfully Moved " + x)
     except:
